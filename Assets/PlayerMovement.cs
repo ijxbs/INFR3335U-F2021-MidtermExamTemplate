15ab68262a47c9987b0eedebc84927908cb2e55e
Jaden <jaden.bhakti@ontariotechu.net>
2021-10-08 19:51:36 -0400

    WASD movement + space to jump

---
+++ Assets/PlayerMovement.cs
@@ -0,0 +1,39 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    public CharacterController controller;
+    public float speed = 7f;
+    public float grav = 9.81f;
+    public float jumpSpeed = 3f;
+    public float dirY;
+
+    // Update is called once per frame
+    void Update()
+    {
+        //WASD movement control
+        float horizontal = Input.GetAxisRaw("Horizontal");
+        float vertical = Input.GetAxisRaw("Vertical");
+        Vector3 direction = new Vector3(horizontal, 0f, vertical);
+
+        //jump control
+        if (controller.isGrounded)
+        {
+            if (Input.GetButtonDown("Jump"))
+            {
+                dirY = jumpSpeed;
+            }
+        }
+
+        dirY -= grav * Time.deltaTime;
+
+        direction.y = dirY;
+
+        if (direction.magnitude >= 0.1f)
+        {
+            controller.Move(direction * speed * Time.deltaTime);
+        }
+    }
+}
